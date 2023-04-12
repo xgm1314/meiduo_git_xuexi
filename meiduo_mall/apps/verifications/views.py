50a2191191d7627c8e8224f553a09787748aa7de
xgm <153094236@qq.com>
2023-04-12 19:16:42 +0800

    短信验证码校验

--- meiduo_mall/apps/verifications/views.py
+++ meiduo_mall/apps/verifications/views.py
@@ -22,7 +22,7 @@ class ImageCodeView(View):
 class SmsCodeView(View):
     ''' 短信验证码 '''
 
-    def get(self, request, mobile):
+    def get_send(self, request, mobile):
         # 获取请求参数
         image_code = request.GET.get('image_code')
         uuid = request.GET.get('image_code_id')
@@ -37,9 +37,23 @@ class SmsCodeView(View):
             return JsonResponse({'code': 400, 'errmsg': '图片验证码已过期'})
         if redis_image_code.decode().lower() != image_code.lower():  # 全部转换成小写进行对比
             return JsonResponse({'code': 400, 'errmsg': '图片验证码错误'})
+        if 'send_flag_%s' % mobile is not None:  # 判断是否有发送短信的标记
+            return JsonResponse({'code': 400, 'errmsg': '操作过于频繁,请勿频繁操作'})
         from random import randint
         sms_code = '%04d' % randint(0, 9999)  # 生成4位随机验证码
         redis_cli.setex(mobile, 120, sms_code)  # 保存验证码
+        redis_cli.setex('send_flag_%s' % mobile, 60, 1)  # 发送短信后,标记发送短信的标记
         from libs.yuntongxun.sms import CCP
         CCP().send_template_sms('17854157598', [sms_code, 2], 1)  # 发送验证码
-        return JsonResponse({'code': 0, 'errmsg': 'ok'})  # 返回响应
+        return JsonResponse({'code': 0, 'errmsg': '验证码发送成功'})  # 返回响应
+
+    def get_receive(self, request, mobile):
+        sms_code_client = request.POST.get('sms_code')  # 获取传入的短信验证码
+        from django_redis import get_redis_connection
+        redis_conn = get_redis_connection('code')  # 连接数据库
+        sms_code_server = redis_conn.get('sms_%s' % mobile)
+        if not sms_code_server:
+            return JsonResponse({'code': 400, 'errmsg': '短信验证码有误'})
+        if sms_code_client != sms_code_server.decode():
+            return JsonResponse({'code': 400, 'errmsg': '短信验证码有误'})
+        return JsonResponse({'code': 0, 'errmsg': '验证成功'})  # 返回响应
