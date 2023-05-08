f7ddd4725b69406e7d1cec6d6c089e3154310749
xgm <153094236@qq.com>
2023-05-08 20:45:41 +0800

    保存支付宝交易信息

--- meiduo_mall/apps/pay/views.py
+++ meiduo_mall/apps/pay/views.py
@@ -130,47 +130,47 @@ class PayUrlView(LoginRequiredJSONMixin, View):
 登录密码111111
 支付密码111111
 """
-# from apps.pay.models import Payment
-#
-#
-# class PaymentStatusView(View):
-#
-#     def put(self, request):
-#         # 1. 接收数据
-#         data = request.GET
-#         # 2. 查询字符串转换为字典 验证数据
-#         data = data.dict()
-#
-#         # 3. 验证没有问题获取支付宝交易流水号
-#         signature = data.pop("sign")
-#
-#         app_private_key_string = open(settings.APP_PRIVATE_KEY_PATH).read()
-#         alipay_public_key_string = open(settings.ALIPAY_PUBLIC_KEY_PATH).read()
-#         # 创建支付宝实例
-#         alipay = AliPay(
-#             appid=settings.ALIPAY_APPID,
-#             app_notify_url=None,  # 默认回调url
-#             app_private_key_string=app_private_key_string,
-#             # 支付宝的公钥，验证支付宝回传消息使用，不是你自己的公钥,
-#             alipay_public_key_string=alipay_public_key_string,
-#             sign_type="RSA2",  # RSA 或者 RSA2
-#             debug=settings.ALIPAY_DEBUG,  # 默认False
-#             config=AliPayConfig(timeout=15)  # 可选, 请求超时时间
-#         )
-#         success = alipay.verify(data, signature)
-#         if success:
-#             # 获取 trade_no	String	必填	64	支付宝交易号
-#             trade_no = data.get('trade_no')
-#             order_id = data.get('out_trade_no')
-#             Payment.objects.create(
-#                 trade_id=trade_no,
-#                 order_id=order_id
-#             )
-#             # 4. 改变订单状态
-#
-#             OrderInfo.objects.filter(order_id=order_id).update(status=OrderInfo.ORDER_STATUS_ENUM['UNSEND'])
-#
-#             return JsonResponse({'code': 0, 'errmsg': 'ok', 'trade_id': trade_no})
-#         else:
-#
-#             return JsonResponse({'code': 400, 'errmsg': '请到个人中心的订单中查询订单状态'})
+from apps.pay.models import Payment
+
+
+class PaymentStatusView(View):
+
+    def put(self, request):
+        # 1. 接收数据
+        data = request.GET
+        # 2. 查询字符串转换为字典 验证数据
+        data = data.dict()
+
+        # 3. 验证没有问题获取支付宝交易流水号
+        signature = data.pop("sign")
+
+        app_private_key_string = open(settings.APP_PRIVATE_KEY_PATH).read()
+        alipay_public_key_string = open(settings.ALIPAY_PUBLIC_KEY_PATH).read()
+        # 创建支付宝实例
+        alipay = AliPay(
+            appid=settings.ALIPAY_APPID,
+            app_notify_url=None,  # 默认回调url
+            app_private_key_string=app_private_key_string,
+            # 支付宝的公钥，验证支付宝回传消息使用，不是你自己的公钥,
+            alipay_public_key_string=alipay_public_key_string,
+            sign_type="RSA2",  # RSA 或者 RSA2
+            debug=settings.ALIPAY_DEBUG,  # 默认False
+            config=AliPayConfig(timeout=15)  # 可选, 请求超时时间
+        )
+        success = alipay.verify(data, signature)
+        if success:
+            # 获取 trade_no	String	必填	64	支付宝交易号
+            trade_no = data.get('trade_no')
+            order_id = data.get('out_trade_no')
+            Payment.objects.create(
+                trade_id=trade_no,
+                order_id=order_id
+            )
+            # 4. 改变订单状态
+
+            OrderInfo.objects.filter(order_id=order_id).update(status=OrderInfo.ORDER_STATUS_ENUM['UNSEND'])
+
+            return JsonResponse({'code': 0, 'errmsg': 'ok', 'trade_id': trade_no})
+        else:
+
+            return JsonResponse({'code': 400, 'errmsg': '请到个人中心的订单中查询订单状态'})
